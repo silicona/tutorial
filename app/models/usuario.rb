60544324689d8ba939bd19e3219b6619299f92ce
Silicona <vertederonuclear@gmail.com>
2017-01-25 08:04:10 +0100

    Cambio de readme

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,13 @@
 class Usuario < ApplicationRecord
+
+	# Relacion entre modelos: Un usuario - Muchas publicaciones
+	# Lado Usuario:
+	has_many :publicaciones
+
+	# nombre y email no pueden estar vacios
+	validates :nombre, :email, presence: true
+
+	# Validacion de los objetos asociados
+	# ¿No esta bien definida?
+	# validates_associated :publicaciones, :message => "Publicacion no válida", on: :create
 end
