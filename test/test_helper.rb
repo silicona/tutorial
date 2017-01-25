1e7f7a05bcde01beb6414e08734ce0b2b6b4feef
Silicona <vertederonuclear@gmail.com>
2017-01-25 12:15:31 +0100

    Capitulo 3 con reporters y guard activo

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# Informe de minitest a colores
+# Informe de barra de progreso, media info.
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
